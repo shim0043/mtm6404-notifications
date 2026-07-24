3ae315c89d35c8480041150cafac3b1763f3d2cc
Shim Kiseoung <shim0043@algonquinlive.com>
2026-07-23 20:33:42 -0400

    Adds state for notification

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ import NotificationItem from './components/NotificationItem';
 import './App.css';
 
 function App() {
-  
+  const [notifications, setNotifications] = useState(notificationsData);
+
+  const clearIndividual = (id) => {
+    const updated = notifications.filter((item) => item.id !== id);
+    setNotifications(updated);
+  };
+
+  const clearAll = () => {
+    setNotifications([]);
+  };
 }
 
 export default App;
